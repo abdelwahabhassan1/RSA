0efda9b3f4deed962b2705ca856124288ef3aba4
abdelwahabhassan1 <ae2200678@tkh.edu.eg>
2024-04-18 13:30:00 +0200

    create 1

---
+++ Program_1.py
@@ -0,0 +1,26 @@
+import random
+import math
+import time
+
+take_bits = int(input("How many bits do you want: "))
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def generate_prime(bits):
+    while True:
+        num = random.getrandbits(bits)
+        if num % 2 != 0:
+            if is_prime(num):
+                return num
