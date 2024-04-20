83fe883d921a475dc6d08715dbb9b25cba7aeaac
abdelwahabhassan1 <ae2200678@tkh.edu.eg>
2024-04-20 12:48:00 +0200

    Update brute force.py

--- brute force.py
+++ brute force.py
@@ -1,2 +1,15 @@
 import random
 take_bits = int(input("How many bits do you want: "))
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def mod_inverse(a, m):
+    m0, x0, x1 = m, 0, 1
+    while a > 1:
+        q = a // m
+        m, a = a % m, m
+        x0, x1 = x1 - q * x0, x0
+    return x1 + m0 if x1 < 0 else x1
